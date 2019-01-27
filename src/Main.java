a0491c444b7b770b52bb259f756bd008d010a459
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-01-27 18:27:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        //Short
+        System.out.println("Short min value " + Short.MIN_VALUE);
+        System.out.println("Short max value " + Short.MAX_VALUE);
+        System.out.println("");
+
+        //Long
+        System.out.println("Long min value " + Long.MIN_VALUE);
+        System.out.println("Long max value " + Long.MAX_VALUE);
+        System.out.println("");
+
+        //Float
+        System.out.println("Float min value " + Float.MIN_VALUE);
+        System.out.println("Float max value " + Float.MAX_VALUE);
+        System.out.println("");
+
+        //Double
+        System.out.println("Double min value " + Double.MIN_VALUE);
+        System.out.println("Double max value " + Double.MAX_VALUE);
+        System.out.println("");
+
+        System.out.println(24.0*0.1);
+        System.out.println(24.0/10.0);
+    }
+}
